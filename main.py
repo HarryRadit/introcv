88242ec61572b2af23567cae4609447a1ece600e
HarryRadit <harrydxm@gmail.com>
2025-01-04 19:26:17 +0700

    computer civon

--- main.py
+++ main.py
@@ -19,14 +19,21 @@ def show_img():
 def show_img2():
     root = tk.Tk()
     root.withdraw()
-    image_path = (filedialog.askopenfilename(title = "Select an image file", filetypes = [("Image files", "*.jpg;*.png;*.jpeg")]))
-    if image_path == "":
-        print("Error 404: unable to load image")
-        return
+    image_path = "nas.jpg"
+    image_path2 = "nas2.jpg"
+    image_path3 = "jas.jpg"
     image = cv2.imread(image_path)
+    image2 = cv2.imread(image_path2)
+    image3 = cv2.imread(image_path3)
     cv2.imshow('Image', image)
+    cv2.imshow('ImageA', image2)
+    cv2.imshow('ImageAA', image3)
     grayscale_image = apply_grayscale(image)
+    grayscale_image2 = apply_grayscale(image2)
+    grayscale_image3 =  apply_grayscale(image3)
     cv2.imshow('Grayscale Image', grayscale_image)
+    cv2.imshow('Grayscale Image', grayscale_image2)
+    cv2.imshow('Grayscale Image', grayscale_image3)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
@@ -37,3 +44,4 @@ def show_img2():
 
 if (__name__ == '__main__'):
     show_img2()
+
---
+++ photo_editor.py
@@ -0,0 +1,31 @@
+import cv2
+
+image_path = 'nas.jpg'
+
+image = cv2.imread(image_path)
+
+def crop_image(image,x,y,w,h):
+    cropped_image = image[y:y+h, x:x+w]
+    return cropped_image
+
+cropped_image = crop_image(image, 0, 0, 900, 200)
+def resize_image(image, scale_percentage):
+    width = int(image.shape[1] * scale_percentage / 100)
+    height = int(image.shape[0] * scale_percentage / 100)
+    resized_image = cv2.resize(image, (width, height))
+    return resized_image
+
+def rotate_image(image, angle):
+    center = (image.shape[1] / 2, image.shape[0] / 2)
+    matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated_image = cv2.warpAffine(image, matrix, (image.shape[1], image.shape[0]))
+
+    return rotated_image
+
+resized_image = resize_image(cropped_image, 70)
+cv2.imshow('Original Image', image)
+cv2.imshow('Cropped Image', resized_image)
+cv2.imshow('Rotated Image', rotate_image(resized_image, 90))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
