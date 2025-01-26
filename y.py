18527197b84d93d77423b566d766005aea00dd89
HarryRadit <harrydxm@gmail.com>
2025-01-26 19:19:22 +0700

    computer civon

--- y.py
+++ y.py
@@ -10,10 +10,10 @@ recognized_text = recognized_text.replace(" ", "").replace("\n", "")
 print("recognized_text")
 print(recognized_text)
 
-_ , binary_image = cv2.threshold(grey_image, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+_ , binary_image = cv2.threshold(grey_image, 150, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 print(binary_image)
 countors,  _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-image_with_contours = cv2.drawContours(image, countors, -1, (0, 0, 255), 1)
+image_with_contours = cv2.drawContours(image, countors, -1, (0, 140,0 ), 1)
 
 cv2.imshow("Image with Contours", image_with_contours)
 cv2.imshow("Binary Image", binary_image)
